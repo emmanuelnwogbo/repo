cd3824b22c122995b6a1240511ece6a402cf99ac
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2020-10-13 12:13:20 +0100

    added lazy loading

--- _nuxt/9c8982b.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{214:function(t,n,l){"use strict";l.r(n);var e={},o=l(15),component=Object(o.a)(e,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"locations"},[this._v("hello locations")])}),[],!1,null,null,null);n.default=component.exports}}]);
