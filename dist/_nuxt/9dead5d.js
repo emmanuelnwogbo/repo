1fc43d0fa297cf0d8da3c7c9037f705967ca047b
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2020-10-15 08:06:36 +0100

    Ican spell CNAME

---
+++ dist/_nuxt/9dead5d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{258:function(t,e,n){"use strict";n.r(e);var l={},c=n(4),component=Object(c.a)(l,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"socialmedia"})}),[],!1,null,null,null);e.default=component.exports}}]);
